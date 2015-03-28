65e9b045f0006254506283d18732ad75f6aa9b11
mziescha <mario_zieschang@web.de>
2015-03-28 18:16:09 +0100

    add simple plugin loader

--- lib/SellSmooth.pm
+++ lib/SellSmooth.pm
@@ -1,15 +1,17 @@
 package SellSmooth;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Module::Load;
 use Dancer2;
+
+use SellSmooth::Plugins;
 
 our $VERSION = '0.1.0';
 
-get '/' => sub {
-    template 'index';
-};
-
-get '/install' => sub {
-
-    template 'install/index';
-};
+my $plugins = SellSmooth::Plugins->new();
+debug Dumper( $plugins->plugins() );
+load $_ foreach @{ $plugins->plugins() };
 
 1;
---
+++ lib/SellSmooth/Base.pm
@@ -0,0 +1,3 @@
+package SellSmooth::Base;
+
+1;
