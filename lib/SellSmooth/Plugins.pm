75489ee023a1ad21f7732f8f3ba67647811fb60b
mziescha <mario_zieschang@web.de>
2015-05-14 10:32:45 +0200

    clean up plugin loader

--- lib/SellSmooth/Plugins.pm
+++ lib/SellSmooth/Plugins.pm
@@ -9,64 +9,60 @@ use YAML::XS qw/LoadFile/;
 use MooseX::HasDefaults::RO;
 use MooseX::Types::Moose qw(ArrayRef Str Defined);
 use MooseX::Types::LoadableClass qw(LoadableClass);
-use Method::Signatures::Simple;
 
 has plugins => ( default => sub { {} } );
-has enabled => ( default => sub { [] } );
 
-has plugin_files => ( isa => 'ArrayRef', is => 'rw', );
-has plugin_name  => ( isa => 'Str',      is => 'rw', );
+has enabled => (
+    default => sub {
+        my $self    = shift;
+        my $enabled = [];
+        foreach ( @{ $self->plugin_files() } ) {
+            my $conf = LoadFile($_);
+            next unless ( $conf->{enabled} );
+            my $class = 'SellSmooth::Plugins::' . $conf->{name};
+            $self->plugin_name( $conf->{name} );
+            $self->plugins()->{ $conf->{name} } =
+              $self->plugin_class()->new( conf => $conf );
+            push( @{$enabled}, $class );
+        }
+        return $enabled;
+    }
+
+);
 
 has plugin_dir => (
-    isa => 'Str',
-
+    isa     => 'Str',
+    lazy    => 1,
     default => sub { File::Spec->catfile( $FindBin::Bin, '..', 'plugins.d' ) }
+);
+
+has plugin_name => (
+    isa => 'Str',
+    is  => 'rw',
 );
 
 has plugin_class => (
     isa     => LoadableClass,
     lazy    => 1,
     is      => 'rw',
-    default => method {
-        'SellSmooth::Plugins::' . $self->plugin_name()
+    default => sub {
+        return 'SellSmooth::Plugins::' . shift->plugin_name();
     }
 );
 
-has plugin_conf => ( is => 'rw', );
-
-has plugin => (
-    does => 'SellSmooth::Plugin',
-    lazy => 1,
-    default =>
-      method { $self->plugin_class()->new( conf => $self->plugin_conf() ); }
+has plugin_files => (
+    isa     => 'ArrayRef',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        my @files = glob( File::Spec->catfile( $self->plugin_dir(), '*.yml' ) );
+        return \@files;
+    }
 );
 
 =head1 SUBROUTINES/METHODS
 
-=head2 BUILD
-
-Run to check driver version with installed db driver.
-
-Creates changelog table if it's not existing.
-
 =cut
-
-sub BUILD {
-    my $self = shift;
-    my @files = glob( File::Spec->catfile( $self->plugin_dir(), '*.yml' ) );
-    $self->plugin_files( \@files );
-    foreach ( @{ $self->plugin_files() } ) {
-        my $conf = LoadFile($_);
-        next unless ( $conf->{enabled} );
-
-        $self->plugin_conf($conf);
-        $self->plugin_name( $conf->{name} );
-        my $class = 'SellSmooth::Plugins::' . $conf->{name};
-
-        $self->plugins()->{ $conf->{name} } = $self->plugin();
-        push( @{ $self->enabled() }, $class );
-    }
-}
 
 no Moose;
 
